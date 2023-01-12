becebb78a844476cfb56c8300abec087e5d87fcd
ilarioforcherio <ilarioforcherio@gmail.com>
2023-01-12 18:30:13 +0100

    push

---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <ul class="navbar-nav d-flex align-items-center ">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><img src="{{asset('images/dc-logo.png')}}" alt="logo"></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CHARACTERS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">COMICS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MOVIES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">TV</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GAMES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">COLLECTIBLES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">VIDEOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FANS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">NEWS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SHOP</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SEARCH</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+  <div class="jumbotron">
+    
+  </div>
